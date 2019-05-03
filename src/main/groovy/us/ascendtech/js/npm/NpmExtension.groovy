9a88f13f0196c79954f7b0bbf3afd33a1a735eae
Matt Davis <matt.davis@ascend-tech.us>
2019-05-03 12:49:39 -0400

    switch default node js to 12.1.0

--- src/main/groovy/us/ascendtech/js/npm/NpmExtension.groovy
+++ src/main/groovy/us/ascendtech/js/npm/NpmExtension.groovy
@@ -14,7 +14,7 @@ class NpmExtension {
     }
 
     //version of node js to download and use
-    String nodeJsVersion = "10.15.1"
+    String nodeJsVersion = "12.1.0"
 
     //this is where additional content is served from in webpack dev mode
     String contentBase = "./src/main/webapp/public/"
