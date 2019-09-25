54e3b00bd266626acb9fa545def230e0752c6576
Matt Davis <matt.davis@ascend-tech.us>
2019-09-25 14:06:48 -0400

    bump version ag-grid to 0.1.9

--- src/main/groovy/us/ascendtech/gwt/lib/GWTLibPlugin.groovy
+++ src/main/groovy/us/ascendtech/gwt/lib/GWTLibPlugin.groovy
@@ -41,8 +41,8 @@ class GWTLibPlugin implements Plugin<Project> {
                 deps.add(project.dependencies.create("javax.annotation:javax.annotation-api:1.3.2"))
             }
             if (gwt.libs.contains("ast-aggrid")) {
-                deps.add(project.dependencies.create('us.ascendtech:agGrid:0.1.8'))
-                deps.add(project.dependencies.create('us.ascendtech:agGrid:0.1.8:sources'))
+                deps.add(project.dependencies.create('us.ascendtech:agGrid:0.1.9'))
+                deps.add(project.dependencies.create('us.ascendtech:agGrid:0.1.9:sources'))
             }
             if (gwt.libs.contains("ast-momentjs")) {
                 deps.add(project.dependencies.create('us.ascendtech:momentjs:0.1.10'))
