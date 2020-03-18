9f2e91cce55d7912c4f578fae22c558f917f3948
Matthew Davis <matt.davis@ascend-tech.us>
2020-03-18 19:11:52 -0400

    bump to vue beta-10 snapshot

--- src/main/groovy/us/ascendtech/gwt/lib/GWTLibPlugin.groovy
+++ src/main/groovy/us/ascendtech/gwt/lib/GWTLibPlugin.groovy
@@ -35,8 +35,8 @@ class GWTLibPlugin implements Plugin<Project> {
             addDependentProjectLibs(project, gwt)
 
             if (gwt.libs.contains("vue")) {
-                deps.add(project.dependencies.create("com.axellience:vue-gwt:1.0-beta-9"))
-                deps.add(project.dependencies.create("com.axellience:vue-router-gwt:1.0-beta-9"))
+                deps.add(project.dependencies.create("com.axellience:vue-gwt:1.0-beta-10-SNAPSHOT"))
+                deps.add(project.dependencies.create("com.axellience:vue-router-gwt:1.0-beta-10-SNAPSHOT"))
                 deps.add(project.dependencies.create("javax.annotation:javax.annotation-api:1.3.2"))
             }
             if (gwt.libs.contains("autorest")) {
