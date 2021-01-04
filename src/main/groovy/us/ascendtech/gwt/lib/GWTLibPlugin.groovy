e369d84e0c5000b10a4ea5950200be0405a9fee7
Matthew Davis <matt.davis@ascend-tech.us>
2021-01-04 16:54:04 -0500

    bump highcharts and aggrid

--- src/main/groovy/us/ascendtech/gwt/lib/GWTLibPlugin.groovy
+++ src/main/groovy/us/ascendtech/gwt/lib/GWTLibPlugin.groovy
@@ -44,8 +44,8 @@ class GWTLibPlugin implements Plugin<Project> {
                 deps.add(project.dependencies.create("javax.annotation:javax.annotation-api:1.3.2"))
             }
             if (gwt.libs.contains("ast-aggrid")) {
-                deps.add(project.dependencies.create('us.ascendtech:agGrid:0.1.10'))
-                deps.add(project.dependencies.create('us.ascendtech:agGrid:0.1.10:sources'))
+                deps.add(project.dependencies.create('us.ascendtech:agGrid:0.1.11'))
+                deps.add(project.dependencies.create('us.ascendtech:agGrid:0.1.11:sources'))
             }
             if (gwt.libs.contains("ast-momentjs")) {
                 deps.add(project.dependencies.create('us.ascendtech:momentjs:0.1.11'))
@@ -57,12 +57,12 @@ class GWTLibPlugin implements Plugin<Project> {
 
             }
             if (gwt.libs.contains("ast-highcharts")) {
-                deps.add(project.dependencies.create('us.ascendtech:highcharts:1.1.8'))
-                deps.add(project.dependencies.create('us.ascendtech:highcharts:1.1.8:sources'))
+                deps.add(project.dependencies.create('us.ascendtech:highcharts:1.1.9'))
+                deps.add(project.dependencies.create('us.ascendtech:highcharts:1.1.9:sources'))
 
                 if (gwt.includeGwtUser) {
-                    deps.add(project.dependencies.create('us.ascendtech:highcharts-injector:1.1.8'))
-                    deps.add(project.dependencies.create('us.ascendtech:highcharts-injector:1.1.8:sources'))
+                    deps.add(project.dependencies.create('us.ascendtech:highcharts-injector:1.1.9'))
+                    deps.add(project.dependencies.create('us.ascendtech:highcharts-injector:1.1.9:sources'))
                 }
             }
             if (gwt.libs.contains("elemento-core-legacy")) {
