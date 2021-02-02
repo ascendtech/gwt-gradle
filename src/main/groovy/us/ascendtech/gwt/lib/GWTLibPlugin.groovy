b59b7e11631b884190c4667a4341f207623ff59a
Payam Meyer <payammeyer@users.noreply.github.com>
2021-02-02 12:29:38 -0500

    Bumped highcharts.

--- src/main/groovy/us/ascendtech/gwt/lib/GWTLibPlugin.groovy
+++ src/main/groovy/us/ascendtech/gwt/lib/GWTLibPlugin.groovy
@@ -57,12 +57,12 @@ class GWTLibPlugin implements Plugin<Project> {
 
             }
             if (gwt.libs.contains("ast-highcharts")) {
-                deps.add(project.dependencies.create('us.ascendtech:highcharts:1.1.13'))
-                deps.add(project.dependencies.create('us.ascendtech:highcharts:1.1.13:sources'))
+                deps.add(project.dependencies.create('us.ascendtech:highcharts:1.1.14'))
+                deps.add(project.dependencies.create('us.ascendtech:highcharts:1.1.14:sources'))
 
                 if (gwt.includeGwtUser) {
-                    deps.add(project.dependencies.create('us.ascendtech:highcharts-injector:1.1.13'))
-                    deps.add(project.dependencies.create('us.ascendtech:highcharts-injector:1.1.13:sources'))
+                    deps.add(project.dependencies.create('us.ascendtech:highcharts-injector:1.1.14'))
+                    deps.add(project.dependencies.create('us.ascendtech:highcharts-injector:1.1.14:sources'))
                 }
             }
             if (gwt.libs.contains("elemento-core-legacy")) {
