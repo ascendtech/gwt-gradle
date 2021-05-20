352b990a42742dbe5a58969f59c96fe10a6091f8
Matthew Davis <matt.davis@ascend-tech.us>
2021-05-20 17:23:33 -0400

    Updated gwt-wordcloud2js version

--- src/main/groovy/us/ascendtech/gwt/lib/GWTLibPlugin.groovy
+++ src/main/groovy/us/ascendtech/gwt/lib/GWTLibPlugin.groovy
@@ -71,8 +71,8 @@ class GWTLibPlugin implements Plugin<Project> {
                 }
             }
             if (gwt.libs.contains("ast-wordcloud")) {
-                deps.add(project.dependencies.create('us.ascendtech:gwt-wordcloud2js:2.0.0'))
-                deps.add(project.dependencies.create('us.ascendtech:gwt-wordcloud2js:2.0.0:sources'))
+                deps.add(project.dependencies.create('us.ascendtech:gwt-wordcloud2js:2.0.2'))
+                deps.add(project.dependencies.create('us.ascendtech:gwt-wordcloud2js:2.0.2:sources'))
             }
 
             if (gwt.libs.contains("elemento-core-legacy")) {
